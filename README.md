bda584ffba3c4e7580503beddd494ca7f6396e07
Alexis Espinoza <alexise857@gmail.com>
2022-06-24 09:18:27 -0500

    primer commit - rest server

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# Socket-Server
+
+Reconstrutir modulos de node
+``
+npm install 
+``
+
+Generar el dist
+``
+ts -w
+``
+
+Levantar servidor: Cualquiera de esos
+``
+nodemon dist/
+``
+``
+node dist/
+``
---
+++ index.ts
@@ -0,0 +1,26 @@
+import Server from "./classes/server";
+import router  from "./routes/router";
+import bodyParser from "body-parser";
+import cors from 'cors'
+
+const server = new Server()
+
+// BodyParser
+// configuración para agregar el middleware de bodyparser
+server.app.use( bodyParser.urlencoded( { extended: true } ) );
+// Pase la petición del formato JSON
+server.app.use( bodyParser.json() )
+
+// CORS
+// Permitiendo que cualquier persona pueda llamar mi servicio
+server.app.use( cors({ origin: true, credentials: true }) )
+
+// Rutas de servicio
+// configuración para usar las rutas
+server.app.use('/', router)
+
+server.start( () => {
+    console.log( ' servidor corriendo en puerto ', server.port )
+} )
+
+
